445c701a2c16b36a37f49531dde9ffb06326eca7
Bhavish-rai <raibhavish007@gmail.com>
2026-07-24 11:57:58 +0530

    Time: 2103 ms (27.07%) | Memory: 41.9 MB (90.91%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+     
+       int k=0,sum=0;
+      
+       for(int i=1;i<num;i++)
+       {
+        if(num%i==0)
+        {
+            sum+=i;
+        }
+       }
+       
+       if(sum==num)
+       {
+        return true;
+       }
+       return false;
+    }
+}
